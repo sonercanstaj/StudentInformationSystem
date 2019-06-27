11af148165c8d4f79cf7c27db9c9cb71c3366844
Can Özdemir <ceaiuaea@gmail..com>
2019-06-27 14:47:19 +0300

    MORTAR EKLENDİ

---
+++ StudentInformationSystem.Model/MORTAR.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.Model
+{
+    class MORTAR
+    {
+        public STUDENT STUDENT { get; set; }
+        public ACADEMİC_SCHEDULE GetACADEMİC_SCHEDULE  {get; set; }
+        public String TOTAL { get; set; }
+        public DateTime PAY_DATE { get; set; }  
+    }
+}
