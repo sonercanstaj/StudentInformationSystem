98a5eca86badbde8cab3e35c30286f52a3900762
Can Özdemir <ceaiuaea@gmail..com>
2019-06-27 12:36:50 +0300

    School Club student sınıfını oluşturdum

---
+++ StudentInformationSystem.Model/SCHOOL_CLUB_STUDENT.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.Model
+{
+    class SCHOOL_CLUB_STUDENT
+    {
+    }
+}
