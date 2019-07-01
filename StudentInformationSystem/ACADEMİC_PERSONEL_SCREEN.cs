c0ca64b13e3aef52d16032e5934ed3db513e1f40
Can Özdemir <ceaiuaea@gmail..com>
2019-07-01 10:32:33 +0300

    SCXZC

--- StudentInformationSystem/ACADEMİC_PERSONEL_SCREEN.cs
+++ StudentInformationSystem/ACADEMİC_PERSONEL_SCREEN.cs
@@ -16,5 +16,15 @@ namespace StudentInformationSystem
         {
             InitializeComponent();
         }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
